eec8f4abb9f072b5c9d88de46f716adb23afa6e8
Rache Bartmoss <bartmoss@tutanota.com>
2020-03-12 16:45:28 +0100

    Cleaned up masking implementation

--- morpheus-core/src/crypto/mask.rs
+++ morpheus-core/src/crypto/mask.rs
@@ -1,5 +1,14 @@
 use failure::{bail, Fallible};
 
+pub fn hash_str(content: &str) -> String {
+    let mut hasher = tiny_keccak::Keccak::new_sha3_256();
+    let mut hash_output = [0u8; 32];
+    hasher.update(content.as_bytes());
+    hasher.finalize(&mut hash_output);
+    let mask = multibase::encode(multibase::Base::Base64Url, &hash_output);
+    format!("cj{}", mask)
+}
+
 pub fn canonical_json(data: &serde_json::Value) -> serde_json::Result<String> {
     match data {
         serde_json::Value::Array(arr) => {
@@ -28,38 +37,17 @@ pub fn canonical_json(data: &serde_json::Value) -> serde_json::Result<String> {
     }
 }
 
-pub fn hashed(content: &str) -> String {
-    let mut hasher = tiny_keccak::Keccak::new_sha3_256();
-    let mut hash_output = [0u8; 32];
-    hasher.update(content.as_bytes());
-    hasher.finalize(&mut hash_output);
-    let mask = multibase::encode(multibase::Base::Base64Url, &hash_output);
-    format!("cj{}", mask)
-}
-
-pub fn hash_json_value(data: &serde_json::Value) -> serde_json::Result<String> {
-    match data {
-        serde_json::Value::Array(_) | serde_json::Value::Object(_) => {
-            let canonical_json = canonical_json(data)?;
-            let digest = hashed(&canonical_json);
-            // println!("Digest of {} is {}", canonical_json, digest);
-            Ok(digest)
-        }
-        _ => canonical_json(data),
-    }
-}
-
-pub fn digest_json_value(data: &serde_json::Value) -> serde_json::Result<serde_json::Value> {
+pub fn collapse_json_subtree(data: &serde_json::Value) -> serde_json::Result<serde_json::Value> {
     match data {
         serde_json::Value::Array(arr) => {
             let mut canonical_json_items = Vec::new();
             for item in arr {
-                let masked_item = digest_json_value(item)?;
+                let masked_item = collapse_json_subtree(item)?;
                 canonical_json_items.push(serde_json::to_string(&masked_item)?);
             }
             let flattened_array = format!("[{}]", canonical_json_items.join(","));
             //println!("Flattened array {} to {}", serde_json::to_string(&data)?, flattened_array);
-            let content_hash = hashed(&flattened_array);
+            let content_hash = hash_str(&flattened_array);
             Ok(serde_json::Value::String(content_hash))
         }
 
@@ -70,14 +58,14 @@ pub fn digest_json_value(data: &serde_json::Value) -> serde_json::Result<serde_j
             for key in keys {
                 let value = obj.get(key).expect("serde_json keys() impl error");
                 let canonical_key = canonical_json(&serde_json::Value::String(key.to_owned()))?;
-                let masked_val = digest_json_value(value)?;
+                let masked_val = collapse_json_subtree(value)?;
                 let entry = format!("{}:{}", canonical_key, serde_json::to_string(&masked_val)?);
                 canonical_json_entries.push(entry);
             }
             // NOTE: braces are escaped as double brace in Rust
             let flattened_object = format!("{{{}}}", canonical_json_entries.join(","));
             //println!("Flattened object {} to {}", serde_json::to_string(&data)?, flattened_object);
-            let content_hash = hashed(&flattened_object);
+            let content_hash = hash_str(&flattened_object);
             Ok(serde_json::Value::String(content_hash))
         }
 
@@ -88,8 +76,8 @@ pub fn digest_json_value(data: &serde_json::Value) -> serde_json::Result<serde_j
 pub fn json_digest<T: serde::Serialize>(data: &T) -> Fallible<String> {
     let json_value = serde_json::to_value(&data)?;
     let digest_json = match &json_value {
-        serde_json::Value::Object(_obj) => digest_json_value(&json_value),
-        serde_json::Value::Array(_arr) => digest_json_value(&json_value),
+        serde_json::Value::Object(_obj) => collapse_json_subtree(&json_value),
+        serde_json::Value::Array(_arr) => collapse_json_subtree(&json_value),
         _ => bail!("Json digest is currently implemented only for composite types"),
     }?;
     match digest_json {
