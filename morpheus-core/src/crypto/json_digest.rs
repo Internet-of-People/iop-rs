d8fa3899f892bc3a262a9394424bb3e9ab7ffa58
Rache Bartmoss <bartmoss@tutanota.com>
2020-03-16 12:40:21 +0100

    Implemented JSON digest with optionally specifying some exceptional branches to be kept unmasked

--- morpheus-core/src/crypto/json_digest.rs
+++ morpheus-core/src/crypto/json_digest.rs
@@ -1,6 +1,10 @@
 use failure::{bail, Fallible};
 
+use crate::util::json_path;
+use std::collections::HashMap;
+
 pub fn hash_str(content: &str) -> String {
+    // TODO we might want to use sha3 crate instead of tiny_keccak
     let mut hasher = tiny_keccak::Keccak::new_sha3_256();
     let mut hash_output = [0u8; 32];
     hasher.update(content.as_bytes());
@@ -9,14 +13,14 @@ pub fn hash_str(content: &str) -> String {
     format!("cj{}", mask)
 }
 
-pub fn canonical_json(data: &serde_json::Value) -> serde_json::Result<String> {
+pub fn canonical_json(data: &serde_json::Value) -> String {
     match data {
         serde_json::Value::Array(arr) => {
             let mut canonical_json_items = Vec::new();
             for item in arr {
-                canonical_json_items.push(canonical_json(item)?);
+                canonical_json_items.push(canonical_json(item));
             }
-            Ok(format!("[{}]", canonical_json_items.join(",")))
+            format!("[{}]", canonical_json_items.join(","))
         }
 
         serde_json::Value::Object(obj) => {
@@ -25,24 +29,27 @@ pub fn canonical_json(data: &serde_json::Value) -> serde_json::Result<String> {
             keys.sort();
             for key in keys {
                 let value = obj.get(key).expect("serde_json keys() impl error");
-                let canonical_key = canonical_json(&serde_json::Value::String(key.to_owned()))?;
-                let entry = format!("{}:{}", canonical_key, canonical_json(value)?);
+                let canonical_key = canonical_json(&serde_json::Value::String(key.to_owned()));
+                let entry = format!("{}:{}", canonical_key, canonical_json(value));
                 canonical_json_entries.push(entry);
             }
             // NOTE: braces are escaped as double brace in Rust
-            Ok(format!("{{{}}}", canonical_json_entries.join(",")))
+            format!("{{{}}}", canonical_json_entries.join(","))
         }
 
-        _ => serde_json::to_string(data),
+        _ => serde_json::to_string(data).expect("serde_json implementation error"),
     }
 }
 
-pub fn collapse_json_subtree(data: &serde_json::Value) -> serde_json::Result<serde_json::Value> {
+pub fn collapse_json_subtree(
+    data: &serde_json::Value, keep_paths: Vec<&str>,
+) -> Fallible<serde_json::Value> {
     match data {
+        // NOTE path expressions are not (yet?) supported for arrays
         serde_json::Value::Array(arr) => {
             let mut canonical_json_items = Vec::new();
             for item in arr {
-                let masked_item = collapse_json_subtree(item)?;
+                let masked_item = collapse_json_subtree(item, vec![])?;
                 canonical_json_items.push(serde_json::to_string(&masked_item)?);
             }
             let flattened_array = format!("[{}]", canonical_json_items.join(","));
@@ -52,38 +59,83 @@ pub fn collapse_json_subtree(data: &serde_json::Value) -> serde_json::Result<ser
         }
 
         serde_json::Value::Object(obj) => {
+            // Build { head => vec![tails] } map
+            let mut keep_head_tails = HashMap::new();
+            for path in keep_paths {
+                let (head, tail_opt) = json_path::split_head_tail(path)?;
+                let tails = keep_head_tails.entry(head.to_owned()).or_insert(Vec::new());
+                if let Some(tail) = tail_opt {
+                    tails.push(tail);
+                }
+            }
+
+            let mut collapse_root = true;
             let mut canonical_json_entries = Vec::new();
             let mut keys: Vec<_> = obj.keys().collect();
             keys.sort();
             for key in keys {
                 let value = obj.get(key).expect("serde_json keys() impl error");
-                let canonical_key = canonical_json(&serde_json::Value::String(key.to_owned()))?;
-                let masked_val = collapse_json_subtree(value)?;
-                let entry = format!("{}:{}", canonical_key, serde_json::to_string(&masked_val)?);
-                canonical_json_entries.push(entry);
+                if let Some(tails) = keep_head_tails.get(key) {
+                    // Found object key present in keep_paths option, skip collapsing current branch of tree
+                    collapse_root = false;
+                    if tails.is_empty() {
+                        // This is the exact Json path to keep open, do not collapse anything
+                        canonical_json_entries.push((key, value.to_owned()));
+                    } else {
+                        // This is a partial match for a Json path to keep open, recurse to collapse it partially
+                        let partial_value = collapse_json_subtree(value, tails.to_owned())?;
+                        canonical_json_entries.push((key, partial_value));
+                    }
+                } else {
+                    // This path does not match any paths, fully collapse
+                    let fully_masked_value = collapse_json_subtree(value, vec![])?;
+                    canonical_json_entries.push((key, fully_masked_value));
+                };
             }
-            // NOTE: braces are escaped as double brace in Rust
-            let flattened_object = format!("{{{}}}", canonical_json_entries.join(","));
-            //println!("Flattened object {} to {}", serde_json::to_string(&data)?, flattened_object);
-            let content_hash = hash_str(&flattened_object);
-            Ok(serde_json::Value::String(content_hash))
+
+            if collapse_root {
+                let canonical_entry_strs = canonical_json_entries
+                    .iter()
+                    // unwrap() also could be .expect("serde_json can't transform its own type into string")
+                    .map(|(key, val)| {
+                        let canonical_key = canonical_json(&serde_json::Value::String(key.to_string()));
+                        format!("{}:{}", canonical_key, serde_json::to_string(val).unwrap())
+                    })
+                    .collect::<Vec<_>>();
+                // NOTE: braces are escaped as double brace in Rust
+                let flattened_object = format!("{{{}}}", canonical_entry_strs.join(","));
+
+                let content_hash = hash_str(&flattened_object);
+                Ok(serde_json::Value::String(content_hash))
+            } else {
+                let mut properties = serde_json::Map::new();
+                for (key, value) in canonical_json_entries {
+                    properties.insert(key.to_owned(), value);
+                }
+                Ok(serde_json::Value::Object(properties))
+            }
         }
 
         _ => Ok(data.clone()),
     }
 }
 
-pub fn json_digest<T: serde::Serialize>(data: &T) -> Fallible<String> {
+pub fn mask_json<T: serde::Serialize>(data: &T, keep_paths: Vec<&str>) -> Fallible<String> {
     let json_value = serde_json::to_value(&data)?;
     let digest_json = match &json_value {
-        serde_json::Value::Object(_obj) => collapse_json_subtree(&json_value),
-        serde_json::Value::Array(_arr) => collapse_json_subtree(&json_value),
+        serde_json::Value::Object(_obj) => collapse_json_subtree(&json_value, keep_paths),
+        serde_json::Value::Array(_arr) => collapse_json_subtree(&json_value, keep_paths),
         _ => bail!("Json digest is currently implemented only for composite types"),
     }?;
     match digest_json {
         serde_json::Value::String(digest) => return Ok(digest),
-        _ => bail!("Implementation error: digest should always return a string"),
+        serde_json::Value::Object(_) => return Ok(canonical_json(&digest_json)),
+        _ => bail!("Implementation error: digest should always return a string or object"),
     }
+}
+
+pub fn json_digest<T: serde::Serialize>(data: &T) -> Fallible<String> {
+    mask_json(data, vec![])
 }
 
 #[cfg(test)]
@@ -106,7 +158,7 @@ mod tests {
     }
 
     #[test]
-    fn masking() -> Fallible<()> {
+    fn test_json_digest() -> Fallible<()> {
         let test_obj = TestData { b: 1, a: 2 };
         {
             let masked = json_digest(&test_obj)?;
@@ -140,6 +192,40 @@ mod tests {
             };
             let masked = json_digest(&comp)?;
             assert_eq!(masked, "cjubdcpA0FfHhD8yEpDzZ8vS5sm7yxkrX_wAJgmke2bWRQ");
+        }
+        Ok(())
+    }
+
+    #[test]
+    fn test_selective_masking() -> Fallible<()> {
+        let test_obj = TestData { b: 1, a: 2 };
+        let composite = CompositeTestData { z: Some(test_obj.clone()), y: Some(test_obj.clone()) };
+        let double_complex =
+            CompositeTestData { z: Some(composite.clone()), y: Some(composite.clone()) };
+        {
+            let fully_masked = mask_json(&composite, vec![])?;
+            assert_eq!(fully_masked, "cjubdcpA0FfHhD8yEpDzZ8vS5sm7yxkrX_wAJgmke2bWRQ");
+        }
+        {
+            let keep_y = mask_json(&composite, vec![".y"])?;
+            assert_eq!(
+                keep_y,
+                r#"{"y":{"a":2,"b":1},"z":"cjumTq1s6Tn6xkXolxHj4LmAo7DAb-zoPLhEa1BvpovAFU"}"#
+            );
+        }
+        {
+            let keep_z = mask_json(&composite, vec![".z"])?;
+            assert_eq!(
+                keep_z,
+                r#"{"y":"cjumTq1s6Tn6xkXolxHj4LmAo7DAb-zoPLhEa1BvpovAFU","z":{"a":2,"b":1}}"#
+            );
+        }
+        {
+            let keep_yz = mask_json(&double_complex, vec![".y.z"])?;
+            assert_eq!(
+                keep_yz,
+                r#"{"y":{"y":"cjumTq1s6Tn6xkXolxHj4LmAo7DAb-zoPLhEa1BvpovAFU","z":{"a":2,"b":1}},"z":"cjubdcpA0FfHhD8yEpDzZ8vS5sm7yxkrX_wAJgmke2bWRQ"}"#
+            );
         }
         Ok(())
     }
---
+++ morpheus-core/src/util/json_path.rs
@@ -0,0 +1,131 @@
+use failure::{bail, Fallible};
+
+// Path pattern syntax is based on JQ patterns, see https://stedolan.github.io/jq/manual/#Basicfilters
+pub fn matches(tree: &serde_json::Value, paths_pattern: &str) -> Fallible<bool> {
+    for single_alternative in split_alternatives(paths_pattern) {
+        if match_single(tree, single_alternative)? {
+            return Ok(true);
+        }
+    }
+    Ok(false)
+}
+
+// Assumes no whitespaces and no alternate paths in parameter
+pub fn match_single(tree: &serde_json::Value, path: &str) -> Fallible<bool> {
+    match tree {
+        serde_json::Value::Object(map) => {
+            let (property_name, path_tail_opt) = split_head_tail(path)?;
+            let path_tail = match path_tail_opt {
+                None => return Ok(map.contains_key(property_name)),
+                Some(path_tail) => path_tail,
+            };
+
+            let property_val_opt = map.get(property_name);
+            match property_val_opt {
+                Some(subtree) => match_single(subtree, path_tail),
+                None => Ok(false),
+            }
+        }
+        // TODO should we support arrays?
+        // serde_json::Value::Array(arr) => {???},
+        _ => Ok(false),
+    }
+}
+
+/// ```
+/// use morpheus_core::util::json_path::split_alternatives;
+/// assert_eq!( split_alternatives(".a , .b.c , .d"), vec![".a", ".b.c", ".d"]);
+/// ```
+pub fn split_alternatives(paths_pattern: &str) -> Vec<&str> {
+    paths_pattern
+        .split_terminator(',') // split alternative tree paths (enabling trailing comma)
+        .map(|item| item.trim()) // trim all items to enable whitespaces near commas
+        .collect()
+}
+
+/// ```
+/// use morpheus_core::util::json_path::split_head_tail;
+/// assert_eq!(split_head_tail(".a").unwrap(), ("a", None));
+/// assert_eq!(split_head_tail(".a.b.c").unwrap(), ("a", Some(".b.c")));
+/// ```
+pub fn split_head_tail(path: &str) -> Fallible<(&str, Option<&str>)> {
+    if !path.starts_with('.') {
+        bail!("Path must start with '.' but it's: {}", path);
+    }
+    let path = &path[1..];
+
+    let dot_idx_opt = path.find('.');
+    let tuple = match dot_idx_opt {
+        None => (path, None),
+        Some(dot_idx) => {
+            let (path_head, path_tail) = path.split_at(dot_idx);
+            (path_head, Some(path_tail))
+        }
+    };
+    Ok(tuple)
+}
+
+#[cfg(test)]
+mod tests {
+    use serde_json::json;
+
+    use super::*;
+
+    fn sample_json_object() -> serde_json::Value {
+        json!({
+            "name": "John Doe",
+            "age": 43,
+            "phones": [
+                "+42 1234567",
+                "+44 2345678"
+            ],
+            "address": {
+                "country": "Germany",
+                "city": "Berlin",
+                "zip": 1234,
+                "street": {
+                    "name": "Some Street",
+                    "number": "1"
+                }
+            }
+        })
+    }
+
+    #[test]
+    fn path_matches() -> Fallible<()> {
+        let obj = sample_json_object();
+
+        assert!(matches(&obj, "invalidpath").is_err());
+        assert_eq!(matches(&obj, ".notpresent").ok(), Some(false));
+        assert_eq!(matches(&obj, ".name").ok(), Some(true));
+        assert_eq!(matches(&obj, ".a").ok(), Some(false));
+        assert_eq!(matches(&obj, ".age").ok(), Some(true));
+        assert_eq!(matches(&obj, ".ageover").ok(), Some(false));
+        assert_eq!(matches(&obj, ".phones").ok(), Some(true));
+        assert_eq!(matches(&obj, ".phones.first").ok(), Some(false));
+        assert_eq!(matches(&obj, ".address").ok(), Some(true));
+        assert_eq!(matches(&obj, ".address.country").ok(), Some(true));
+        assert_eq!(matches(&obj, ".address.city").ok(), Some(true));
+        assert_eq!(matches(&obj, ".address.street").ok(), Some(true));
+        assert_eq!(matches(&obj, ".address.street.name").ok(), Some(true));
+        assert_eq!(matches(&obj, ".address.street.number").ok(), Some(true));
+        assert_eq!(matches(&obj, ".address.street.none").ok(), Some(false));
+        assert_eq!(matches(&obj, ".address.phone").ok(), Some(false));
+
+        assert!(matches(&obj, ".none , invalid , .ageover ").is_err());
+        assert_eq!(matches(&obj, ".none , .fake , .ageover ").ok(), Some(false));
+        assert_eq!(matches(&obj, ".none , .fake , .ageover , .age").ok(), Some(true));
+        assert_eq!(matches(&obj, ".addr , .address.street.num, .xxx").ok(), Some(false));
+        assert_eq!(matches(&obj, ".addr , .address.street.number, .xxx").ok(), Some(true));
+        assert_eq!(matches(&obj, ".ad , .address.street.numbers, .xxx").ok(), Some(false));
+
+        Ok(())
+    }
+
+    #[test]
+    fn selective_json_masking() -> Fallible<()> {
+        let _obj = sample_json_object();
+        // TODO
+        Ok(())
+    }
+}
