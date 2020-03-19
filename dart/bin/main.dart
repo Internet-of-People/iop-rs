2d39ef9986cd88731eb98d9670223153da614d2f
Sandor Muranyi <contact@mudlee.hu>
2020-03-19 10:51:34 +0100

    using dart packages from the sdk now

--- dart/bin/main.dart
+++ dart/bin/main.dart
@@ -1,6 +1,6 @@
 import 'dart:io' show Platform, File;
 
-import 'package:morpheus_dart/rust.dart';
+import 'package:morpheus_sdk/crypto.dart';
 
 void main(List<String> arguments) {
   /*try {
@@ -25,68 +25,62 @@ void main(List<String> arguments) {
     print('Vault loading is implemented for Linux only yet.');
   }
 
+  final sdk = CryptoAPI.create('../target/debug/libmorpheus_sdk.so');
   try {
-    final sdk = RustAPI.initSdk('../target/debug/libmorpheus_sdk.so');
+    final nonce = sdk.generateNonce();
+    print('Generated nonce: $nonce');
+
+    final contentId = 'cjuzC-XxgzNMwYXtw8aMIAeS2Xjlw1hlSNKTvVtUwPuyYo';
+    final maskedContentId = sdk.maskJson('"${contentId}"', '.');
+    print('Masking string is idempotent: ${contentId == maskedContentId}');
+
+    final json = File('bin/witnessStatement.json').readAsStringSync();
+    final maskedJson = sdk.maskJson(json, '.claim.content.dateOfBirth');
+    print('Masked Json:\n$maskedJson');
+
+    final vaultPath =
+        '${Platform.environment['HOME']}/.config/prometheus/did_vault.dat';
     try {
-      final nonce = sdk.generateNonce();
-      print('Generated nonce: $nonce');
-
-      final contentId = 'cjuzC-XxgzNMwYXtw8aMIAeS2Xjlw1hlSNKTvVtUwPuyYo';
-      final maskedContentId = sdk.maskJson('"${contentId}"', '.');
-      print('Masking string is idempotent: ${contentId == maskedContentId}');
-
-      final json = File('bin/witnessStatement.json').readAsStringSync();
-      final maskedJson = sdk.maskJson(json, '.claim.content.dateOfBirth');
-      print('Masked Json:\n$maskedJson');
-
-      final vaultPath =
-          '${Platform.environment['HOME']}/.config/prometheus/did_vault.dat';
-      try {
-        sdk.loadVault(vaultPath);
-      } catch (e) {
-        sdk.createVault(
-            'include pear escape sail spy orange cute despair witness trouble sleep torch wire burst unable brass expose fiction drift clock duck oxygen aerobic already',
-            vaultPath);
-      }
-
-      while (sdk.listDids().length < 2) {
-        print('Created ${sdk.createDid()}');
-      }
-
-      final dids = sdk.listDids();
-      print('Dids: ${dids.join(',')}');
-
-      final witnessRequest = File('bin/witnessRequest.json').readAsStringSync();
-      final witnessRequestId = sdk.maskJson(witnessRequest, '.');
-      final signedWitnessRequest = sdk.signWitnessRequest(witnessRequest, 'iezbeWGSY2dqcUBqT8K7R14xr');
-      print('Signed Witness Request:\n$signedWitnessRequest');
-      print('Witness Request Content ID: ${witnessRequestId}');
-
-      final witnessStatement = File('bin/witnessStatement.json').readAsStringSync();
-      final signedWitnessStatement = sdk.signWitnessStatement(witnessStatement, 'iezbeWGSY2dqcUBqT8K7R14xr');
-      print('Signed Witness Statement:\n$signedWitnessStatement');
-
-      final claimPresentation = File('bin/claimPresentation.json').readAsStringSync();
-      final signedClaimPresentation = sdk.signClaimPresentation(claimPresentation, 'iezbeWGSY2dqcUBqT8K7R14xr');
-      print('Signed Claim Presentation:\n$signedClaimPresentation');
-
-      // sdk.fakeLedger();
-      sdk.realLedger('http://35.187.56.222:4703');
-
-      // final doc1 = sdk.getDocument(dids[0]);
-      // print('first document: \n$doc1');
-
-      final isTombstoned = sdk.isTombstonedAt(dids[0], 126);
-      print('tombstoned: ${isTombstoned}');
-
-      final hasRight = sdk.hasRightAt(dids[0], 'iezbeWGSY2dqcUBqT8K7R14xr', 'impersonate', 126);
-      print('did has right: ${hasRight}');
+      sdk.loadVault(vaultPath);
     } catch (e) {
-      print('Error using SDK: $e');
-    } finally {
-      sdk.dispose();
+      sdk.createVault(
+          'include pear escape sail spy orange cute despair witness trouble sleep torch wire burst unable brass expose fiction drift clock duck oxygen aerobic already',
+          vaultPath);
     }
-  } catch (e) {
-    print('Error initializing SDK: $e');
+
+    while (sdk.listDids().length < 2) {
+      print('Created ${sdk.createDid()}');
+    }
+
+    final dids = sdk.listDids();
+    print('Dids: ${dids.join(',')}');
+
+    final witnessRequest = File('bin/witnessRequest.json').readAsStringSync();
+    final witnessRequestId = sdk.maskJson(witnessRequest, '.');
+    final signedWitnessRequest = sdk.signWitnessRequest(witnessRequest, 'iezbeWGSY2dqcUBqT8K7R14xr');
+    print('Signed Witness Request:\n$signedWitnessRequest');
+    print('Witness Request Content ID: ${witnessRequestId}');
+
+    final witnessStatement = File('bin/witnessStatement.json').readAsStringSync();
+    final signedWitnessStatement = sdk.signWitnessStatement(witnessStatement, 'iezbeWGSY2dqcUBqT8K7R14xr');
+    print('Signed Witness Statement:\n${signedWitnessStatement.toJson()}');
+
+    final claimPresentation = File('bin/claimPresentation.json').readAsStringSync();
+    final signedClaimPresentation = sdk.signClaimPresentation(claimPresentation, 'iezbeWGSY2dqcUBqT8K7R14xr');
+    print('Signed Claim Presentation:\n${signedClaimPresentation.toJson()}');
+
+    // sdk.fakeLedger();
+    sdk.realLedger('http://35.187.56.222:4703');
+
+    // final doc1 = sdk.getDocument(dids[0]);
+    // print('first document: \n$doc1');
+
+    final isTombstoned = sdk.isTombstonedAt(dids[0], 126);
+    print('tombstoned: ${isTombstoned}');
+
+    final hasRight = sdk.hasRightAt(dids[0], 'iezbeWGSY2dqcUBqT8K7R14xr', 'impersonate', 126);
+    print('did has right: ${hasRight}');
+  } finally {
+    sdk.dispose();
   }
 }
