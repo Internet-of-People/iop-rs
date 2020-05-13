1376d0a5d0e40456ff4051419521853653e40e40
wigy <wigy_opensource_developer@yahoo.com>
2020-05-13 16:36:49 +0200

    Make an npm package that combines node and browser targets

--- morpheus-core-wasm/build.sh
+++ morpheus-core-wasm/build.sh
@@ -1,7 +1,21 @@
+#!/bin/bash
+
 # Prerequisites:
 # - OpenSSH headers, e.g. sudo apt install libssh-dev
 # - clang, e.g. sudo apt install clang
 # - wasm-pack binary: e.g. cargo install wasm-pack
-# - optinally wasm-opt to autooptimize binaries: e.g. sudo apt install binaryen
+# - optionally wasm-opt to autooptimize binaries: e.g. sudo apt install binaryen
+
 cargo build
-wasm-pack build --target nodejs
+
+rm -rf pkg/
+wasm-pack build --target browser --out-dir pkg/browser --out-name iop_morpheus_core_wasm
+wasm-pack build --target nodejs --out-dir pkg/node --out-name iop_morpheus_core_wasm
+
+mv pkg/browser/*.d.ts pkg/
+rm pkg/browser/.gitignore
+rm pkg/browser/package.json
+rm pkg/node/*.d.ts
+rm pkg/node/.gitignore
+rm pkg/node/package.json
+cp .package.json pkg/package.json
