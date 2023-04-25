f9fa08bda5f5db59be3f1de2960c94ef0a37f54c
mudlee <contact@mudlee.hu>
2023-04-25 16:14:06 +0200

    Added SignedBytes's constructor to ffi

--- sdk-ffi/src/signed/bytes.rs
+++ sdk-ffi/src/signed/bytes.rs
@@ -6,6 +6,18 @@ pub extern "C" fn delete_SignedBytes(signed: *mut Signed<Box<[u8]>>) {
 }
 
 #[no_mangle]
+pub extern "C" fn SignedBytes_new(
+    pk: *const MPublicKey, content: *const CSlice<u8>, sig: *const MSignature,
+) -> *mut Signed<Box<[u8]>> {
+    let pk = unsafe { convert::borrow_in(pk) };
+    let content = unsafe { convert::borrow_in(content) };
+    let sig = unsafe { convert::borrow_in(sig) };
+    let signed_bytes =
+        Signed::new(pk.clone(), content.as_slice().to_owned().into_boxed_slice(), sig.clone());
+    convert::move_out(signed_bytes)
+}
+
+#[no_mangle]
 pub extern "C" fn SignedBytes_public_key_get(signed: *mut Signed<Box<[u8]>>) -> *mut MPublicKey {
     public_key(signed)
 }
